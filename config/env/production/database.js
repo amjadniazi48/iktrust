5450ca852aee894f3e1ada362c2bf65bc3ce25be
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-03-23 20:16:41 +0500

    Added production db file for configuration

---
+++ config/env/production/database.js
@@ -0,0 +1,18 @@
+    // strapi-api/config/database.js
+    module.exports = ({ env }) => ({
+        connection: {
+          client: 'postgres',
+          connection: {
+            host: env('PGHOST', 'localhost'),
+            port: env.int('PGPORT', 50570),
+            database: env('PGDATABASE', 'railway'),
+            user: env('PGUSER', 'postgres'),
+            password: env('PGPASSWORD', 'amPhfunSnZCEWDjUbmRezAIcfCnBdlTi'),
+        
+            ssl: {
+              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+            },
+          },
+          debug: false,
+        },
+      });
