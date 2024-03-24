be44cec1fddb86771d20549a0b9cdd454ba575d6
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-03-24 15:16:25 +0500

    Changes in the databse.ts file for server

--- config/database.ts
+++ config/database.ts
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "iktrust"),
+      port: env.int("DATABASE_PORT", 50570),
+      database: env("DATABASE_NAME", "postgres"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "postgres"),
+      password: env("DATABASE_PASSWORD", "amPhfunSnZCEWDjUbmRezAIcfCnBdlTi"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
